23bfd1160a97e9a5daff0b1832e31a8709a2cd5b
AmirZukhair2 <amir-z@zenrin-tokai.co.jp>
2025-04-05 22:47:24 +0900

    new hero photo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
                   transition={{duration: 1}}>
         <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
           <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-            <Image src="/hero.png" alt="" fill className="object-contain"/>
+            <Image src="/hero.jpg" alt="" fill className="object-contain"/>
           </div>
           <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
             <h1 className="text-4xl md:text-6xl font-bold">Hello</h1>
